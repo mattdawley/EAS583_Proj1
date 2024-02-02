20c3ae6d4b622b67e2ceeb7defc6338e0c1672a1
Matt Dawley <dawley@gmail.com>
2024-02-01 18:45:07 -0700

    attempt 1

--- gen_keys.py
+++ gen_keys.py
@@ -13,11 +13,15 @@ def get_keys(challenge,keyId = 0, filename = "eth_mnemonic.txt"):
     If fewer than (keyId+1) mnemonics have been generated, generate a new one and return that
     """
 
+    private_key = "46e2623ed463a8523bbc9aea7c1e732fe9768ae9d1c4b5026fc7196252055a42"
+    wallet_address = "0x99ECb0aBBa20B98Cf096496841241ed5e8a90883"
+
     w3 = Web3()
+    w3.eth.default_account = wallet_address
+    eth_addr = w3.eth.account.from_key(private_key)
 
     msg = eth_account.messages.encode_defunct(challenge)
-
-	#YOUR CODE HERE
+    sig = eth_addr.sign_message(msg, private_key)
 
     assert eth_account.Account.recover_message(msg,signature=sig.signature.hex()) == eth_addr, f"Failed to sign message properly"
 
